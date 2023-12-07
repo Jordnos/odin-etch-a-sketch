dade988c1671f4d51fd77e5ec8c078478ddfda81
Jordnos <jordy7789@hotmail.com>
2023-12-07 01:37:02 -0800

    Finished initial page
    - on grid hover it changes colour to yellow
    - user can create new sketchpad

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+let gridSize = 16;
+
+function createDivs() {
+    let container = document.querySelector(".container");
+    for (let i = 0; i < gridSize; i++) {
+        let col = document.createElement("div");
+        col.classList.add("grid-col");
+        container.append(col);
+    }
+    let cols = document.querySelectorAll(".grid-col");
+    cols.forEach( col => {
+        for (let i = 0; i < gridSize; i++) {
+            let div = document.createElement("div");
+            div.classList.add("square");
+            col.append(div);
+        }
+    });
+}
+
+function deleteSketchPadContents(node) {
+    while (node.hasChildNodes()) {
+        deleteSketchPadContents(node.firstChild);
+    }
+    node.parentNode.removeChild(node);
+}
+
+function deleteCurrentSketchPad() {
+    let container = document.querySelector(".container");
+    while (container.hasChildNodes()) {
+        deleteSketchPadContents(container.firstChild);
+    }
+}
+
+function addEventListeners() {
+    document.querySelectorAll(".square").forEach(square => {
+        square.addEventListener("mouseenter", () => {
+            square.classList.add("hovered");
+        })
+    });
+
+    document.querySelector(".new-grid-button").addEventListener("click", () => {
+        gridSize = prompt("What size sketchpad: ");
+        while (gridSize > 100) {
+            gridSize = prompt("Please choose a size smaller than 100: ");
+        }
+        deleteCurrentSketchPad();
+        init();
+    })
+}
+
+function init() {
+    createDivs();
+    addEventListeners();
+}
+
+init();
