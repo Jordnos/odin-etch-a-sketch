fbe8d2e3f12693a62ce1e584f2a6ad18800ff649
Jordnos <jordy7789@hotmail.com>
2023-12-09 01:19:03 -0800

    Add change brush functionality
    - added buttons for different brushes

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,11 @@
+const COLOR_BRUSH = 1;
+const ERASE_BRUSH = 2;
+const LIGHTEN_BRUSH = 3;
+const DARKEN_BRUSH = 4;
+const RAINBOW_BRUSH = 5;
+
 let gridSize = 16;
+let brush = COLOR_BRUSH;
 
 function createDivs() {
     let container = document.querySelector(".container");
@@ -31,26 +38,36 @@ function deleteCurrentSketchPad() {
     }
 }
 
-function randomColour() {
+function getRandomColour() {
     let r = Math.floor(Math.random()*(226));
     let g = Math.floor(Math.random()*(226));
     let b = Math.floor(Math.random()*(226));
     return [r, g, b];
 }
 
-function addEventListeners() {
+function addBrushEventListener() {
     document.querySelectorAll(".square").forEach(square => {
         square.addEventListener("mouseenter", () => {
-            let colour = randomColour();
-            square.style.background = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]})`;
-            if (square.classList.contains("hovered")) {
-                let currentBrightness = window.getComputedStyle(square).getPropertyValue("filter").match(/\((.*)\)/)[1];
-                square.style.filter = `brightness(${currentBrightness-0.1})`;
-            } else {
-                square.classList.add("hovered");
+            let currentBrightness = window.getComputedStyle(square).getPropertyValue("filter").match(/\((.*)\)/)[1];
+            if (brush === RAINBOW_BRUSH) {
+                let colour = getRandomColour();
+                square.style.background = `rgb(${colour[0]}, ${colour[1]}, ${colour[2]})`;
+            } else if (brush === DARKEN_BRUSH) {
+                square.style.filter = `brightness(${parseFloat(currentBrightness) - 0.1})`;
+            } else if (brush === LIGHTEN_BRUSH && currentBrightness < 1) {
+                square.style.filter = `brightness(${parseFloat(currentBrightness) + 0.1})`;
+            } else if (brush === COLOR_BRUSH) {
+                square.style.background = "yellow";
+            } else if (brush === ERASE_BRUSH) {
+                square.style.background = "white";
+                square.style.filter = `brightness(${1})`;
             }
         })
     });
+}
+
+function addEventListeners() {
+    addBrushEventListener();
 
     document.querySelector(".new-grid-button").addEventListener("click", () => {
         gridSize = prompt("What size sketchpad: ");
@@ -59,7 +76,23 @@ function addEventListeners() {
         }
         deleteCurrentSketchPad();
         init();
-    })
+    });
+
+    document.querySelector(".colour-brush-button").addEventListener("click", () => {
+        brush = COLOR_BRUSH;
+    });
+
+    document.querySelector(".erase-brush-button").addEventListener("click", () => {
+        brush = ERASE_BRUSH;
+    });
+
+    document.querySelector(".darken-brush-button").addEventListener("click", () => {
+        brush = DARKEN_BRUSH;
+    });
+
+    document.querySelector(".lighten-brush-button").addEventListener("click", () => {
+        brush = LIGHTEN_BRUSH;
+    });
 }
 
 function init() {
